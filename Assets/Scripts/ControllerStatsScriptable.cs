9d50603965bd451bc20a39b1d66907e8c782be19
Pacific2104 <>
2024-08-11 23:32:48 +0530

    Initial commit with Controller

---
+++ Assets/Scripts/ControllerStatsScriptable.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ControllerStatsScriptable : ScriptableObject
+{
+    [Space]
+    [Tooltip("Players layer mask to ignore in raycast")]
+    public LayerMask playerLayer;
+    [Tooltip("Configure input deadzone to deal with stick drift on controllers")]
+    public float deadZone;
+
+    [Space(2), Header("Movement")]
+    [Tooltip("Max speed player can reach")]
+    public float maxSpeed;
+    [Tooltip("Values by which player will accelerate to max speed")]
+    public float acceleration;
+    [Tooltip("Values by which player will deaccelerate on ground")]
+    public float groundDeceleration;
+    [Tooltip("Values by which player will deaccelerate in air")]
+    public float airDeceleration;
+
+    [Space(2), Header("Jumps")]
+    [Tooltip("Players jump velocity")]
+    public float jumpPower;
+    [Tooltip("Constante downward force applied while the player is grounded (helps with edges)")]
+    public float groundingAcceleration;
+    [Tooltip("Constante downward force applied while the player is in air (snappier jump)")]
+    public float fallAcceleration;
+    [Tooltip("Players max fall velocity")]
+    public float maxFallSpeed;
+    public float jumpBuffer;
+    public float jumpEndEarlyMultiplier;
+    [Tooltip("Time buffer where player can jump after leaving the edge")]
+    public float coyoteTime;
+}   
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField, NaughtyAttributes.Expandable] ControllerStatsScriptable stats;
+
+    Vector2 _inputVelocity;
+    Vector2 _currentVelocity;
+    float _deceleration;
+    bool _grounded = true;
+    bool _inCoyoteTime = true;
+    bool _jumpEndEarly;
+
+    Coroutine _jumpCoroutine = null;
+    Coroutine _coyoteCoroutine = null;
+
+    float _time = 0;
+    float _jumpPressTime = 0;
+    float _jumpReleaseTime = 0;
+
+    Collider2D _col;
+    Rigidbody2D _rb;
+    CharacterInputs _controller;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>() ? GetComponent<Rigidbody2D>() : gameObject.AddComponent<Rigidbody2D>();
+        _rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        _col = GetComponent<Collider2D>() ? GetComponent<Collider2D>() : gameObject.AddComponent<Collider2D>();
+    }
+
+    private void OnEnable()
+    {
+        if (_controller == null)
+        {
+            _controller = new();
+            _controller.Character.Walk.performed += i => _inputVelocity = i.ReadValue<Vector2>();
+            _controller.Character.Jump.performed += i => { Jump(); };
+            _controller.Character.Jump.canceled += i =>  { JumpEnd(); _jumpReleaseTime = _time; };
+        }
+        _controller.Enable();
+    }
+    private void OnDisable()
+    {
+        _controller.Disable();
+        StopAllCoroutines();
+    }
+    private void Update()
+    {
+        CheckGrounded();
+        CheckCeiling();
+        HorizontalVelocity();
+        Gravity();
+
+        _time += Time.deltaTime;
+    }
+    void HorizontalVelocity()
+    {
+        if (Mathf.Abs(_inputVelocity.x) < stats.deadZone)
+        {
+            _deceleration = _grounded ? stats.groundDeceleration : stats.airDeceleration;
+            _currentVelocity.x = Mathf.MoveTowards(_currentVelocity.x, 0, _deceleration * Time.deltaTime);
+        }
+        else
+        {
+            _currentVelocity.x = Mathf.MoveTowards(_currentVelocity.x, stats.maxSpeed * _inputVelocity.x, stats.acceleration * Time.deltaTime);
+        }
+        _rb.velocity = _currentVelocity;
+    }
+    void Jump()
+    {
+        if (_grounded && stats.jumpBuffer <= (_time - _jumpPressTime))
+        {
+            _jumpPressTime = _time;
+            _jumpEndEarly = false;
+            JumpEnd();
+            _jumpCoroutine = StartCoroutine(JumpRoutine());
+        }
+    }
+    void JumpEnd()
+    {
+        if (_jumpCoroutine != null)
+        {
+            StopCoroutine(_jumpCoroutine);
+            _jumpCoroutine = null;
+            if (_rb.velocity.y > 0 && (_jumpPressTime - _jumpReleaseTime) < 1.0f)
+            {
+                _jumpEndEarly = true;
+            }
+        }
+    }
+    IEnumerator JumpRoutine()
+    {
+        float t = 0f;
+        while (t < 0.1f)
+        {
+            t += Time.deltaTime;
+            _currentVelocity.y = stats.jumpPower;    
+            yield return null;
+        }
+    }
+    void Gravity()
+    {
+        if (_grounded && !_inCoyoteTime)
+        {
+            _currentVelocity.y = -stats.groundingAcceleration;
+        }
+        else
+        {
+            if (_jumpEndEarly)
+            {
+                _currentVelocity.y = Mathf.MoveTowards(_currentVelocity.y, -stats.maxFallSpeed, stats.fallAcceleration * Time.deltaTime * stats.jumpEndEarlyMultiplier);
+            }
+            else
+            {
+                _currentVelocity.y = Mathf.MoveTowards(_currentVelocity.y, -stats.maxFallSpeed, stats.fallAcceleration * Time.deltaTime);
+            }
+        }
+    }
+    void CheckGrounded()
+    {
+        if (Physics2D.CircleCast(_col.bounds.center, _col.bounds.size.x / 2, Vector2.down, _col.bounds.size.y / 2 - 0.15f, ~stats.playerLayer))
+        { 
+            if (_coyoteCoroutine != null && !_grounded)
+            {
+                StopCoroutine(_coyoteCoroutine);
+                _coyoteCoroutine = null;
+                _grounded = true;
+                _inCoyoteTime = false;
+                _jumpEndEarly = false;
+            }
+        }
+        else
+        {
+            _coyoteCoroutine ??= StartCoroutine(CoyoteTime());           
+        }
+    }
+    void CheckCeiling()
+    {
+        if (Physics2D.CircleCast(_col.bounds.center, _col.bounds.size.x / 2, Vector2.up, _col.bounds.size.y / 2 - 0.15f, ~stats.playerLayer))
+        {
+            if (_rb.velocity.y > 0)
+            {
+                _currentVelocity.y = Mathf.MoveTowards(_currentVelocity.y, 0, stats.fallAcceleration * Time.deltaTime);
+                JumpEnd();
+                _jumpEndEarly = true;
+            }
+        }
+    }
+    IEnumerator CoyoteTime()
+    {
+        _inCoyoteTime = true;
+        yield return new WaitForSeconds(stats.coyoteTime);
+        _grounded = false;
+        _inCoyoteTime = false;
+    }
+}
